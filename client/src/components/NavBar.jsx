4f7647a1b0c56add345e19a4442e3630a4f668a9
randomologist <randomologist2000@gmail.com>
2025-10-19 15:52:56 -0500

    Navbar in somewhat complete state

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,15 +4,27 @@ import './NavBar.css'
 function NavBar() {
     return(
         <nav className='nav-container'>
-            <a className='nav-brand' href="/">Library</a>
-            <div className='nav-links'>
-                <NavLink to="/example" className = {({ isActive }) => (isActive? 'nav-link active' : 'nav-link')}>
-                    Example
-                </NavLink>
-                <NavLink to="/example2" className = {({ isActive }) => (isActive? 'nav-link active' : 'nav-link')}>
-                    Example2
-                </NavLink>
-            </div>
+            <nav className='util-bar-container'>
+                <div className='bar-links'>
+                    <NavLink to="/help" className = {({ isActive }) => (isActive? 'bar-link active' : 'bar-link')}>
+                        ? Help
+                    </NavLink>
+                    <NavLink to="/login" className = {({ isActive }) => (isActive? 'bar-link active' : 'bar-link')}>
+                        ~ Log-in
+                    </NavLink>
+                </div>
+            </nav>
+            <nav className = 'sub-nav-container'>
+                <a className='nav-brand' href="/">🏠Library</a>
+                <div className='sub-nav-links'>
+                    <NavLink to="/example" className = {({ isActive }) => (isActive? 'sub-nav-link active' : 'sub-nav-link')}>
+                        Example
+                    </NavLink>
+                    <NavLink to="/example2" className = {({ isActive }) => (isActive? 'sub-nav-link active' : 'sub-nav-link')}>
+                        Example2
+                    </NavLink>
+                </div>
+            </nav>
         </nav>
     )
 }
